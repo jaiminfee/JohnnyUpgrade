6c4e75cc23361cc5e4d747591a8960a3be188443
jaiminfee <151575712+jaiminfee@users.noreply.github.com>
2023-11-20 15:44:02 -0500

    Add files via upload

---
+++ Term project.py
@@ -0,0 +1,22 @@
+from cmu_graphics import *
+
+class Johnny:
+    def __init__(self):
+        pass
+
+def onAppStart(app):
+    app.level = 0
+
+def drawFractal(level, cx, cy, r):
+    pass
+
+def onKeyPress(app, key):
+    pass
+
+def redrawAll(app):
+    pass
+
+def main():
+    runApp()
+
+main()
